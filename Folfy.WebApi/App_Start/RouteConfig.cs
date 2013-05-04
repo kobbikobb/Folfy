530fc20b76dda0d483dfb9eb2e9a2d8fb3471b06
kobbikobb <jakob.jonasson@gmail.com>
2013-05-04 11:48:07 +0000

    Main page

--- Folfy.WebApi/App_Start/RouteConfig.cs
+++ Folfy.WebApi/App_Start/RouteConfig.cs
@@ -15,7 +15,8 @@ namespace Folfy.WebApi
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}"
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
